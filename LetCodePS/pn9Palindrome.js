fdecf68a3f729c7891c30139c3dcd76e6db58e1f
RusulAzomSumon <18103020@iubat.edu>
2022-11-20 23:48:28 +0600

    pallindrome number

---
+++ LetCodePS/pn9Palindrome.js
@@ -0,0 +1,44 @@
+// 9. Palindrome Number
+// link: https://leetcode.com/problems/palindrome-number/
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+
+//  
+
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+
+// Example 2:
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+// Example 3:
+
+// Input: x = 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+//  
+
+// Constraints:
+
+//     -2e31<= x <= 2e31 - 1
+
+// Solution: 
+var x;
+
+ function isPalindrome(x){
+    if(x>0){
+        return true;
+    }else{
+        return false;
+    }
+    
+};
+
+// output 
+console.log(isPalindrome(212));
